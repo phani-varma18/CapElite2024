e222b59d5dbccca77c2503cd26d375899a93a627
phani-varma18 <85873549+phani-varma18@users.noreply.github.com>
2023-01-27 16:17:05 +0530

    Leetcode 226

---
+++ 27-01-23/Invert Binary Tree(BinaryTrees).java
@@ -0,0 +1,22 @@
+class Solution {
+    public TreeNode invertTree(TreeNode root) 
+    {
+        helper(root);
+        return root;
+    }
+
+    public static void helper(TreeNode root)
+    {
+        if(root==null)
+        {
+            return;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        helper(root.left);
+        helper(root.right);
+    }
+}
