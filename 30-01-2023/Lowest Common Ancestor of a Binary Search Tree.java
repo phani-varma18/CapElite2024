3d30857b6d99d3811c43c37dc15d4c1a4277ae01
phani-varma18 <85873549+phani-varma18@users.noreply.github.com>
2023-01-30 16:22:32 +0530

    Leetcode 235

---
+++ 30-01-2023/Lowest Common Ancestor of a Binary Search Tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) 
+    {
+        if(root==null)
+        {
+            return null;
+        }
+
+        if(root.val==p.val || root.val==q.val || (root.val>q.val && root.val<p.val) || (root.val>p.val && root.val<q.val))
+        {
+            return root;
+        }
+
+        if(root.val>p.val && root.val>q.val)
+        {
+            return lowestCommonAncestor(root.left,p,q);
+        }
+
+        return lowestCommonAncestor(root.right,p,q);
+    }
+}
